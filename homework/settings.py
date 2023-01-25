06b730dbcc3c6d86e2dcf38fb802881b1190f9b6
Rohan Panchal <rohan@rohanpanchal.com>
2023-01-25 17:13:02 -0600

    Added students template.

--- homework/settings.py
+++ homework/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'homework.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'homework/templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -84,6 +85,11 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+# Test Database
+import sys
+if 'test' in sys.argv or 'test_coverage' in sys.argv: #Covers regular testing and django-coverage
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 
 # Password validation
--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Student, Course
 
 # Create your tests here.
+
+class StudentTestCase(TestCase):
+    def setUp(self):
+        Student.objects.create(first_name="Test_First_Name", last_name="Test_Last_Name")
+        Course.objects.create(department="CSC", number="394")
+
+
+    def test_students_have_courses(self):
+
+        course = Course.objects.get(id=1)
+
+        student = Student.objects.get(id=1)
+
+        student.student_courses.add(course)
